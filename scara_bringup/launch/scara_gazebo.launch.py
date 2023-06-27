3cd90584ae61be87ffa46f98573052f75d5d144d
shambu9r <103073635+shambu9r@users.noreply.github.com>
2023-06-27 19:10:02 +0530

    Add files via upload

--- scara_bringup/launch/scara_gazebo.launch.py
+++ scara_bringup/launch/scara_gazebo.launch.py
@@ -14,6 +14,8 @@
 
 from launch import LaunchDescription
 from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
@@ -33,23 +35,10 @@ def generate_launch_description():
     )
     robot_description = {'robot_description': robot_description_content}
 
-    robot_controllers = PathJoinSubstitution(
-        [
-            FindPackageShare('scara_description'),
-            'config',
-            'scara_controllers.yaml',
-        ]
-    )
     rviz_config_file = PathJoinSubstitution(
         [FindPackageShare('scara_description'), 'rviz', 'scara.rviz']
     )
 
-    control_node = Node(
-        package='controller_manager',
-        executable='ros2_control_node',
-        parameters=[robot_description, robot_controllers],
-        output='both',
-    )
     robot_state_pub_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -62,6 +51,23 @@ def generate_launch_description():
         name='rviz2',
         output='log',
         arguments=['-d', rviz_config_file],
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [PathJoinSubstitution(
+                [FindPackageShare('gazebo_ros'),
+                    'launch', 'gazebo.launch.py']
+            )]
+        ),
+        launch_arguments={'verbose': 'false'}.items(),
+    )
+
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic', robot_description, '-entity', 'scara'],
+        output='screen',
     )
 
     joint_state_broadcaster_spawner = Node(
@@ -88,16 +94,16 @@ def generate_launch_description():
         package='slider_publisher', 
         executable='slider_publisher', 
         name='slider_publisher',
-        parameters=[{'rate': 10.0}],
         arguments = [slider_config])
 
     nodes = [
-        rviz_node,
-        control_node,
+        gazebo,
         robot_state_pub_node,
-        # joint_state_broadcaster_spawner,
-        # robot_controller_spawner,
-        # slider_node,
+        spawn_entity,
+        rviz_node,
+        joint_state_broadcaster_spawner,
+        robot_controller_spawner,
+        slider_node
     ]
 
     return LaunchDescription(nodes)
